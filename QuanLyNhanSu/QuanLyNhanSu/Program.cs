b93a6626c85be33c8524499ee03bb1f21e5e3230
TrangHuyen <tmr.huyentrang@gmail.com>
2018-06-11 14:07:27 +0700

    edit

--- QuanLyNhanSu/QuanLyNhanSu/Program.cs
+++ QuanLyNhanSu/QuanLyNhanSu/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLyNhanSu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GUI.TienLuong());
+            Application.Run(new GUI.LoginForm());
         }
     }
 }
